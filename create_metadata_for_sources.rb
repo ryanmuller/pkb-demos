862d6e684cd46f18d85259a19e57200a5bd48b4b
Ryan Muller <cognitionmachine@gmail.com>
2015-03-06 19:24:38 -0600

    more robust source content grabbing

--- create_metadata_for_sources.rb
+++ create_metadata_for_sources.rb
@@ -4,7 +4,9 @@ require "sqlite3"
 require "json"
 require "optparse"
 require "open-uri"
+require "openssl"
 require "readability"
+require "zlib"
 
 def configuration
   path = File.join File.dirname(__FILE__), "secrets.yml"
@@ -23,7 +25,7 @@ def prismatic_interests(url)
     resp = conn.post "/url/topic", url: url
     body = JSON.parse(resp.body)
     (body["topics"] || []).map { |t| t["topic"] }.join ","
-  rescue Faraday::TimeoutError, JSON::ParserError
+  rescue Faraday::TimeoutError, JSON::ParserError, Faraday::ConnectionFailed
     ""
   end
 end
@@ -33,7 +35,7 @@ def content_html(url)
   begin
     source = open(url).read
     Readability::Document.new(source).content
-  rescue URI::InvalidURIError, RuntimeError, OpenURI::HTTPError, SocketError
+  rescue URI::InvalidURIError, RuntimeError, OpenURI::HTTPError, SocketError, OpenSSL::SSL::SSLError, Errno::ETIMEDOUT, Zlib::BufError
     ""
   end
 end
@@ -61,16 +63,16 @@ db.execute("select url from sources").map { |s| s[0] }.each do |url|
 end
 
 # update interests
-db.execute("select url from source_metadata where interests is null").map { |s| s[0] }.each do |url|
-  db.execute "update source_metadata set interests=? where url=?", prismatic_interests(url), url
-end
-
-# update interest sources
-db.execute("select url, interests from source_metadata").each do |source|
-  source[1].split(",").each do |interest|
-    db.execute "insert into interest_sources(interest, url) values (?,?)", interest, source[0]
-  end
-end
+#db.execute("select url from source_metadata where interests is null").map { |s| s[0] }.each do |url|
+#  db.execute "update source_metadata set interests=? where url=?", prismatic_interests(url), url
+#end
+#
+## update interest sources
+#db.execute("select url, interests from source_metadata").each do |source|
+#  source[1].split(",").each do |interest|
+#    db.execute "insert into interest_sources(interest, url) values (?,?)", interest, source[0]
+#  end
+#end
 
 # update content
 db.execute("select url from source_metadata where content_html is null").map { |s| s[0] }.each do |url|
